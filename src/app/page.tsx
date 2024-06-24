23fd5519bbb38269306d4d0fad6f7e9806fdae31
mletemps <mletemps@hotmail.com>
2024-06-24 08:56:25 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { Resource } from "sst";
+import Chat from "@/components/chat";
+import styles from "../components/chat.module.css";
+
+const topic = "sst-chat";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+
+            <div>
+                <Chat
+                    endpoint={Resource.MyRealtime.endpoint}
+                    authorizer={Resource.MyRealtime.authorizer}
+                    topic={`${Resource.App.name}/${Resource.App.stage}/${topic}`}
+                />
+            </div>
+        </main>
+    );
+}
